0ba04aceede7727efcdfb28ec602bed2dfba8d67
Taleb Shaikh <talebshaikh34343434@gmail.com>
2025-04-26 21:33:17 +0530

    Fixed dependencies

--- hotair-balloon-frontend/vite.config.js
+++ hotair-balloon-frontend/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from 'path'; // Need to import path
-
+import path from "path"; // Need to import path
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve('./', './src'), // Assuming your source code is in 'src'
+      "@": path.resolve("./", "./src"), // Assuming your source code is in 'src'
       // Or if "@/"" points to the root:
       // '@': path.resolve(__dirname, './')
     },
